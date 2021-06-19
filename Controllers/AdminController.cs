15e2a7f10653735f76e31b7b60201a6e47c575b6
nightfury45 <agentp207@gmail.com>
2021-06-19 16:20:43 +0700

    - attempting to manually merge with quang-dev - Add Edit and Delete trainee account Functions and Views

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -11,6 +11,7 @@ using TrainingManagementSystem.Models;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.DataProtection;
 using Microsoft.Ajax.Utilities;
+using TrainingManagementSystem.ViewModels;
 
 namespace TrainingManagementSystem.Controllers
 {
@@ -31,26 +32,14 @@ namespace TrainingManagementSystem.Controllers
         {
             return RedirectToAction("Index", "Home");
         }
-        [HttpGet]
-        public ActionResult ShowTrainer()
-        {
-           var users = _context.Users.ToList();
-            var trainers = new List<ApplicationUser>();
-            foreach (var user in users)
-            {
-                if (_userManager.GetRoles(user.Id)[0].Equals("trainer"))
-                {
-                    trainers.Add(user);
-                }
-            }
-            return View(trainers);
-        }
+
         [Authorize(Roles = "admin")]
         [HttpGet]
         public ActionResult ChangePassword()
         {
             return View();
         }
+
         [Authorize (Roles = "admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -87,6 +76,22 @@ namespace TrainingManagementSystem.Controllers
             }
             return View(staffs);
         }
+
+        [HttpGet]
+        public ActionResult ShowTrainer()
+        {
+            var users = _context.Users.ToList();
+            var trainers = new List<ApplicationUser>();
+            foreach (var user in users)
+            {
+                if (_userManager.GetRoles(user.Id)[0].Equals("trainer"))
+                {
+                    trainers.Add(user);
+                }
+            }
+            return View(trainers);
+        }
+
         [HttpGet]
         public ActionResult ShowTrainee(string searchString)
         {
@@ -102,6 +107,7 @@ namespace TrainingManagementSystem.Controllers
             }
             return View(trainee);
         }
+
         public ActionResult TrainerDetails(string id)
         {
             if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
@@ -137,5 +143,173 @@ namespace TrainingManagementSystem.Controllers
 
             return View(trainee);
         }
+
+        [HttpGet]
+        public ActionResult StaffEdit(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var staff = _context.Users
+                .SingleOrDefault(s => s.Id == id);
+            if (staff == null) return HttpNotFound();
+
+            var viewModel = new StaffViewModel()
+            {
+                User = staff,
+
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult StaffEdit(ApplicationUser user)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new StaffViewModel()
+                {
+                    User = user,
+                };
+                return View(viewModel);
+            }
+
+            var staff = _context.Users
+                .SingleOrDefault(s => s.Id == user.Id);
+
+            if (staff == null) return HttpNotFound();
+
+            staff.Email = user.Email;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+
+        }
+
+        [HttpGet]
+        public ActionResult TrainerEdit(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var trainerInDb = _context.Trainers
+                .SingleOrDefault(t => t.UserId == id);
+
+            if (trainerInDb == null) return HttpNotFound();
+
+            var viewModel = new TrainerViewModel()
+            {
+                Trainer = trainerInDb,
+
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult TrainerEdit(Trainer trainer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new TrainerViewModel()
+                {
+                    Trainer = trainer,
+                };
+                return View(viewModel);
+            }
+
+            var trainerInDb = _context.Trainers
+                .SingleOrDefault(t => t.UserId == trainer.UserId);
+
+            if (trainerInDb == null) return HttpNotFound();
+
+            trainerInDb.Name = trainer.Name;
+            trainerInDb.Type = trainer.Type;
+            trainerInDb.Email = trainer.Email;
+            trainerInDb.WorkPlace = trainer.WorkPlace;
+            trainerInDb.Phone = trainer.Phone;
+            trainerInDb.Education = trainer.Education;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+
+        }
+        [HttpGet]
+        public ActionResult TraineeEdit(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var traineeInDb = _context.Trainees
+                .SingleOrDefault(t => t.UserId == id);
+
+            if (traineeInDb == null) return HttpNotFound();
+
+            var viewModel = new TraineeViewModel()
+            {
+                Trainee = traineeInDb,
+
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult TraineeEdit(Trainee trainee)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new TraineeViewModel()
+                {
+                    Trainee = trainee,
+                };
+                return View(viewModel);
+            }
+
+            var traineeInDb = _context.Trainees
+                .SingleOrDefault(t => t.UserId == trainee.UserId);
+
+            if (traineeInDb == null) return HttpNotFound();
+
+            traineeInDb.Name = trainee.Name;
+            traineeInDb.Email = trainee.Email;
+            traineeInDb.Education = trainee.Education;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+
+        }
+        public ActionResult StaffDelete(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var del = _context.Users
+                .SingleOrDefault(d => d.Id == id);
+
+            if (del == null) return HttpNotFound();
+
+            _context.Users.Remove(del);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult TrainerDelete(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var del = _context.Trainers
+                .SingleOrDefault(d => d.UserId == id);
+            var del2 = _context.Users
+                .SingleOrDefault(d => d.Id == id);
+
+            if (del == null) return HttpNotFound();
+
+            _context.Trainers.Remove(del);
+            _context.Users.Remove(del2);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
---
+++ ViewModels/TrainerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrainingManagementSystem.Models;
+
+namespace TrainingManagementSystem.ViewModels
+{
+    public class TrainerViewModel
+    {
+        public Trainer Trainer { get; set; }
+
+    }
+}
